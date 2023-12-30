0837e6082a96305227899f480d42dd910d986d37
patobeur41 <patobeur41@gmail.com>
2023-12-30 13:07:53 +0100

    clean

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ const expressServer = app.listen(PORT, () => {
 const io = new Server(expressServer, {
 	cors: {
 		origin: process.env.NODE_ENV === "production" ? false : [
-			"http://192.168.17.125:3500",
 			"http://localhost:5500",
 			"http://127.0.0.1:5500"
 		]
